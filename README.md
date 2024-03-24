ff81e1e95eceeedd3a833d1555bab005e1036e94
sadahussain123567 <systemhang377@gmail.com>
2024-03-24 03:14:40 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cli-number-guessing-game
+ this is a cli based number guessing game
---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// computer se random number generate karwaen
+//input from user
+const randomnumber = Math.floor(Math.random() * 6 + 1);
+//console.log(randomnumber)
+const answers = await inquirer.prompt([
+    {
+        name: "userguessednumber",
+        type: "number",
+        message: "please guess a number between 1 to 6",
+    },
+]);
+if (answers.userguessednumber === randomnumber) {
+    console.log("congratulation you got the true number.");
+}
+else {
+    console.log("you guessed wrong number");
+}
